3bda9fd14661827947c4c63051672da5805ee4e7
bannastre <bannastre@gmail.com>
2017-05-09 21:51:26 +0100

    Ch.11 Tests passed - instance variables added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,13 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bike
+
   def release_a_bike
     Bike.new
   end
 
   def dock_a_bike(bike)
+    @bike = bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,23 @@
 require "docking_station"
 
 describe DockingStation  do
-  it { is_expected.to respond_to :release_a_bike }
+  it { is_expected.to respond_to(:release_a_bike) }
   it { is_expected.to respond_to(:dock_a_bike).with(1).argument }
+  it { is_expected.to respond_to(:bike) }
 
   it 'Releases working bikes' do
     bike = subject.release_a_bike
     expect(bike).to be_bike_is_working
+  end
+
+  it 'Docks a bike' do
+    bike = Bike.new
+    expect(subject.dock_a_bike(bike)).to eq bike
+  end
+
+  it 'returns the docked bike' do
+    bike = Bike.new
+    subject.dock_a_bike(bike)
+    expect(subject.bike).to eq bike
   end
 end
